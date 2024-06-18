e1ed86153ee346e4b1968a7cc33e932b14c3dafd
masroof-maindak <62666332+masroof-maindak@users.noreply.github.com>
2024-06-18 13:24:26 +0500

    Create swamp.lua

---
+++ extras/lite-xl/colors/swamp.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#242015" }
+style.background2 = { common.color "#211d13" }
+style.background3 = { common.color "#211d13" }
+style.text = { common.color "#856a5d" }
+style.caret = { common.color "#F1E9D0" }
+style.accent = { common.color "#ebe0bb" }
+style.dim = { common.color "#856a5d" }
+style.divider = { common.color "#211d13" }
+style.selection = { common.color "#3A3124" }
+style.line_number = { common.color "#856a5d" }
+style.line_number2 = { common.color "#ebe0bb" }
+style.line_highlight = { common.color "#32302f" }
+style.scrollbar = { common.color "#856a5d" }
+style.scrollbar2 = { common.color "#F1E9D0" }
+
+style.syntax["normal"] = { common.color "#ebe0bb" }
+style.syntax["symbol"] = { common.color "#ebe0bb" }
+style.syntax["comment"] = { common.color "#856a5d" }
+style.syntax["keyword"] = { common.color "#a82d56" }
+style.syntax["keyword2"] = { common.color "#80465f" }
+style.syntax["number"] = { common.color "#b05d61" }
+style.syntax["literal"] = { common.color "#b05d61" }
+style.syntax["string"] = { common.color "#736f4e" }
+style.syntax["operator"] = { common.color "#ebe0bb" }
+style.syntax["function"] = { common.color "#db930d" }
